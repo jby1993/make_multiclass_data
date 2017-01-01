d8d5581b66d03a20edd7c7aecb0be3c16b42c1c1
jby <jby1993@mail.ustc.edu.cn>
2017-01-01 13:46:32 +0800

    use multi thread and my render to rende patch data on server, solved some bad code, use large useless time. I am so stupidgit status!

--- source/main.cpp
+++ source/main.cpp
@@ -7,28 +7,39 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "io_utils.h"
 #include "make_data_for_multi_classlearn.h"
-#include <QDir>
+#include <omp.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
 
-//    if(argc!=4)
-//    {
-//        std::cout<<"input 3 para, one is data root, the other is save root!"<<std::endl;
-//        return -1;
-//    }
+    if(argc!=5)
+    {
+        std::cout<<"input 4 para, one is thread num, second is if render_to_label, third is data root, the firth is save root!"<<std::endl;
+        return -1;
+    }
     make_data_for_multi_classlearn make;
-//    if(std::string(argv[1])=="true")
-//        make.set_render_to_label(true);
-//    else if(std::string(argv[1])=="false")
+    std::string num = argv[1];
+    std::stringstream ss;
+    ss<<num;
+    int thread_num=-1;
+    ss>>thread_num;
+    if(thread_num<=0)
+    {
+        std::cout<<"thread num input is wrong!!! input greater than 0 integer."<<std::endl;
+        return -1;
+    }
+    else
+        make.set_thread_num(thread_num);
+    if(std::string(argv[2])=="true")
         make.set_render_to_label(true);
-//    else
-//    {
-//        std::cout<<"label flag input wrong!!!"<<std::endl;
-//        return -1;
-//    }
-//    make.make_data(argv[2],argv[3]);
-        make.make_data("../../multi_learn_data/data0/","../../patch_result/");
-
+    else if(std::string(argv[2])=="false")
+        make.set_render_to_label(false);
+    else
+    {
+        std::cout<<"label flag input wrong!!!"<<std::endl;
+        return -1;
+    }
+    make.make_data(argv[3],argv[4]);
 
 
 
